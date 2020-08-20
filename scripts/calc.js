4d7a9189329885193ff7c100b3c16ec2ed8c9e83
lucas <lucas.s.s.silva@hotmail.com>
2020-08-19 23:43:29 -0300

    inicializando

---
+++ scripts/calc.js
@@ -0,0 +1,10 @@
+// Orientação a objetos e MVC
+
+// Quanto mais o código for otimizado, melhor.
+
+// MVC = Model View Controller
+// Model = controla os dados
+// View = o que o cliente vê (layout)
+// Controller = define a regra de negócio
+
+let calculadora = new CalcController;
---
+++ scripts/controller/CalcController.js
@@ -0,0 +1,4 @@
+class CalcController {
+    // Dentro de uma classe encontra-se variáveis(atributo) e funções(método)
+    
+};

a791c23925d8bcb40b1d633fac283ab7b17b1cb1
lucas <lucas.s.s.silva@hotmail.com>
2020-08-31 16:59:35 -0300

    finalizando

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,10 +1,1 @@
-// Orientação a objetos e MVC
-
-// Quanto mais o código for otimizado, melhor.
-
-// MVC = Model View Controller
-// Model = controla os dados
-// View = o que o cliente vê (layout)
-// Controller = define a regra de negócio
-
-window.calculator = new CalcController();
+let calculadora = new CalcController();
